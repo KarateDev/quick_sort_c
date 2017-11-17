49d443406b8010d8fdc9c97603f8fa46d1b55e64
Unknown <clement.sorin@orange.fr>
2017-11-17 16:44:45 +0100

    Le code
    Mon code un peu commenté

---
+++ quickSort_util.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int array[], int a, int b){
+  int temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
+}
+
+void initPivot(int array[], int first, int last){
+  int max = last;
+  if(array[max] < array[first]){
+    max = first;
+  }
+  if(array[max] < array[last/2]){
+    max = last/2;
+  }
+  swap(array, max, last);
+}
+
+
+void quickSort_int_wrk(int array[], int first, int last) {
+  int head = first;
+  int pivot = last;
+
+  if(first>=last){
+    return;
+  }else{
+    //tri
+    while (pivot != head){
+      //si la vlauer pivot est plus petite ou égale a
+      //la valeur de pointeur, echanger les valeurs
+      if (array[pivot] <= array[head]){
+        swap(array, pivot, head);
+      }
+      // si le pivot est plus grand que le pointeur
+      //incrémenter le pointeur
+      if (pivot > head) {
+        head++;
+      }else{
+        //sinon decrémenter le pointeur
+        head--;
+      }
+    }
+    //appel recursif
+    quickSort_int_wrk(array, first, pivot-1);
+    quickSort_int_wrk(array, pivot, last);
+  }
+
+  return;
+}
+
+void quickSort_int(int array[], int size){
+  if(size < 2){
+    return;
+  }else{
+    initPivot(array,0,size-1);
+    quickSort_int_wrk(array,0,size-1);
+    return;
+  }
+}
---
+++ quickSort_util.h
@@ -0,0 +1,9 @@
+void swap(int array[], int a, int b)
+
+int getLength(int array[])
+
+void initPivot(int array[], int first, int last)
+
+void quickSort_int(int array[])
+
+void quickSort_int_wrk(int array[], int first, int last)
